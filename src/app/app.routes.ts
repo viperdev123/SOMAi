6661d96b35ff23d1ebaf4352fd7790955ddc9774
T01147 <T01147@cdg.co.th>
2026-02-08 17:02:37 +0700

    create landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { CreatePage } from '../createPage/create-page/create-page';
 import { Review } from '../review/review/review';
 import { HistoryPage } from '../history/history-page/history-page';
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashBoard },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: DashBoard },
   { path: 'create', component: CreatePage },
   { path: 'reviews', component: Review },
   { path: 'history', component: HistoryPage }
--- src/dashBoard/dash-board/dash-board.ts
+++ src/dashBoard/dash-board/dash-board.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { SideBar } from '../../sideBar/side-bar/side-bar';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+import { LottieComponent } from 'ngx-lottie';
 @Component({
   selector: 'app-dash-board',
   imports: [
     SideBar,
+    ButtonModule,
+    LottieComponent
   ],
   templateUrl: './dash-board.html',
   styleUrl: './dash-board.css',
 })
 export class DashBoard {
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  goToCreatePage() {
+    this.router.navigate(['/create']);
+  }
+
+  lottieOptions = {
+    path: 'assets/lottie/ai_animation_Flow.json',
+    loop: true,
+    autoplay: true
+  };
 
 }
